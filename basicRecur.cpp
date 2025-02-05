becf6485211f5fca17b5464417f4bbbaf6239251
MIRFANY <mohdirfan68883@gmail.com>
2025-02-05 20:47:03 +0530

    Ist commit

---
+++ basicRecur.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int factorial(int n){
+    if(n==0){
+        return 1;
+    }
+
+    int chotti= factorial(n-1);
+    int badi = n* chotti;
+    return badi;
+}
+int main(){
+    int n;
+    cin>>n;
+    int ans= factorial(n);
+    cout<< ans <<endl;
+}
