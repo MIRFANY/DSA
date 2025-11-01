5c6b323315f8f536d8637fb8ce039dd05c0373a1
Irfan <mohdirfan68883@gmail.com>
2025-11-01 23:25:33 +0530

    Add files via upload

--- bfs.cpp
+++ bfs.cpp
@@ -65,3 +65,39 @@ int main() {
     cout << endl;
     return 0;
 }
+
+
+
+
+
+// rough 
+
+int main(){
+    unordered_map<int, list<int> adjList;
+    vector<int> ans;
+    unordered_map<int, bool> visited;
+
+    prepareAdjList(adjList, edges);
+
+
+    //have to check  or traverse all components
+    for(int i=0; i< vertex; i++){
+        if(!visited[i]){
+            bfs(adjList, visited, ans, i);
+        }
+    }
+
+    return ans ;
+
+
+    for(int i=0;i<edges.size(); i++ {
+        int u= edges[i].first;
+        int v = edges[i].second;
+
+        adjList[u].push_back(v);
+        adjList[v].push_back(u);
+
+
+    })
+
+}
